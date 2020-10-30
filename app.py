efd76e4af453adb4a49e9022634817e9367740f2
jayasurya <69354118+notjayasurya@users.noreply.github.com>
2020-10-30 09:38:51 +0530

    Add files via upload
    Linked two sample webpages using flask. Learning the basics.

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [{
+		"name":"jayasurya",
+		"runs_scored": "25364",
+		"average":"63",
+		"batting":"Left hand batsman",
+		},
+		{
+		"name":"Virat Kohli",
+		"runs_scored": "65764",
+		"average":"55",
+		"batting":"Right hand batsman",
+		},
+]
+
 @app.route('/')
 def home():
-	return "Vanakkam Ulagame"
+	return render_template("home.html", posts = posts)
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)

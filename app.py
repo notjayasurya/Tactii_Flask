084161e996de17a6970c25988a4938f8bd9976f2
jayasurya <69354118+notjayasurya@users.noreply.github.com>
2020-10-30 14:46:27 +0530

    Extended flask functions

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
 posts = [{
-		"name":"jayasurya",
-		"runs_scored": "25364",
-		"average":"63",
-		"batting":"Left hand batsman",
+		"name":"Name: Jayasurya Karthikeyan",
+		"runs_scored": "Runs Scored: 25364",
+		"average":"Average : 63",
+		"batting":"Batting: Left hand batsman",
 		},
 		{
-		"name":"Virat Kohli",
-		"runs_scored": "65764",
-		"average":"55",
-		"batting":"Right hand batsman",
+		"name":"Name : Virat Kohli",
+		"runs_scored": "Runs Scored: 65764",
+		"average":"Average: 55",
+		"batting":"Batting: Right hand batsman",
 		},
 ]
 
@@ -22,7 +22,7 @@ def home():
 
 @app.route('/about')
 def about():
-	return render_template("about.html")
+	return render_template("about.html", title = 'ABOUT')
 
 if __name__ == "__main__":
 	app.run(debug = True)
